100d478bae292d784b49360cba174ca724cf3d9a
TophatGuy99 <133228638+TophatGuy99@users.noreply.github.com>
2023-08-28 15:31:38 +0800

    Initial Commit

---
+++ L8/L8App.swift
@@ -0,0 +1,17 @@
+//
+//  L8App.swift
+//  L8
+//
+//  Created by Michael De Beyer on 28/8/23.
+//
+
+import SwiftUI
+
+@main
+struct L8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
